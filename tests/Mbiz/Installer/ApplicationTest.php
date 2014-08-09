da547400e19f56d84ab66b45f63bb8f59dbd4d68
Dopamedia <testa.peta@gmail.com>
2014-08-09 10:29:24 +0200

    add test-folder && testing-bootstrap-file

---
+++ tests/Mbiz/Installer/ApplicationTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mothership
+ * Date: 09/08/14
+ * Time: 10:22
+ */
+
+namespace Mbiz\Installer;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+
+}
